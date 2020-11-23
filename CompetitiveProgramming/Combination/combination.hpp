c3d94fe4ae2c2bfcea78f9bf860010606e6ca15f
hs-nazuna <h.suzuki.lskp@gmail.com>
2020-11-23 22:45:45 +0900

    rename methods

--- CompetitiveProgramming/Combination/combination.hpp
+++ CompetitiveProgramming/Combination/combination.hpp
@@ -23,29 +23,32 @@ private:
     
     void precomputation() {
         fact[0] = fact[1] = 1;
-        for (int i=2; i<=N; ++i) fact[i] = (fact[i-1] * i) % MOD;
-        for (int i=0; i<=N; ++i) inv[i] = powmod(fact[i], MOD-2);
+        for (int i=2; i<=N; ++i) fact[i] = (fact[i - 1] * i) % MOD;
+        for (int i=0; i<=N; ++i) inv[i] = powmod(fact[i], MOD - 2);
     }
     
 public:
     Combination(int N, lint MOD) : N(N), MOD(MOD), fact(N + 1), inv(N + 1) {
         precomputation();
     }
+
+    lint P(int n, int k) {
+        if (k == 0) return 1;
+        if (n < k) return 0;
+        return (fact[n] * inv[n - k]) % MOD;
+    }
     
-    lint compute(int n, int k, bool duplicate = false) {
-        if (duplicate) return compute(n+k-1, k);
+    lint C(int n, int k) {
         if (k == 0) return 1;
         if (n < k) return 0;
         lint res = fact[n];
-        res = (res * inv[n-k]) % MOD;
+        res = (res * inv[n - k]) % MOD;
         res = (res * inv[k]) % MOD;
         return res;
     }
 
-    lint compute_P(int n, int k) {
-        if (k == 0) return 1;
-        if (n < k) return 0;
-        return (fact[n] * inv[n - k]) % MOD;
+    lint H(int n, int k) {
+        return (n + k - 1, k);
     }
 };
 
