1ba3fde9c4b1d067986f5243a0f03daffa501ae2
hs-nazuna <h.suzuki.lskp@gmail.com>
2020-11-23 22:48:07 +0900

    size_t -> int

--- CompetitiveProgramming/SlidingMinimum/sliding_minimum.hpp
+++ CompetitiveProgramming/SlidingMinimum/sliding_minimum.hpp
@@ -3,16 +3,12 @@
 
 #include <vector>
 #include <deque>
-#include <cassert>
 
 template<typename T>
-std::vector<size_t> sliding_minimum(const std::vector<T>& tar, size_t K) {
-    assert(K > 0);
-    
-    std::vector<size_t> ret;
-    
-    std::deque<size_t> deq;
-    for (size_t i=0, n=tar.size(); i<n; ++i) {
+std::vector<int> sliding_minimum(const std::vector<T>& tar, int K) {
+    std::vector<int> ret;    
+    std::deque<int> deq;
+    for (int i=0, n=tar.size(); i<n; ++i) {
         while (!deq.empty() and tar[deq.back()] >= tar[i]) deq.pop_back();
         deq.push_back(i);
         if (i >= K - 1) {
@@ -20,7 +16,6 @@ std::vector<size_t> sliding_minimum(const std::vector<T>& tar, size_t K) {
             if (deq.front() + K - 1 == i) deq.pop_front();
         }
     }
-    
     return ret;
 }
 
--- CompetitiveProgramming/SlidingMinimum/test.cpp
+++ CompetitiveProgramming/SlidingMinimum/test.cpp
@@ -14,7 +14,7 @@ int main() {
     for (int i=0; i<n; ++i) cout << v[i] << (i<n-1 ? " " : "\n");
     cout << "Window Size : " << K << endl;
     
-    vector<size_t> sm = sliding_minimum(v, K);
+    vector<int> sm = sliding_minimum(v, K);
     
     cout << "Index : ";
     for (int i=0; i<n-K+1; ++i) cout << sm[i] << (i<n-K ? " " : "\n");
